1b64b039dde4847baa2facbfc85fe2af0db39414
Dylan DE JESUS <dyl.dejesusmilitar@gmail.com>
2023-04-28 18:46:27 +0200

    Rules module cleared

--- inc/regles.h
+++ inc/regles.h
@@ -2,15 +2,20 @@
 
 #include "../inc/types.h"
 
-
+/* Checks if the player is still alive */
 int isAlive();
 
+/* Adds a single user life point */
 void addLife();
 
+/* Remove a single user life point */
 void removeLife();
 
+/* Retrieves the end information on the corridor axis */
 double endCorridor();
 
+/* Initialize the length of the corridor */
 void initLengthCorridor();
 
+/* Checks if the racket position given is out of the corridor */
 int isCorridorEnd(double racket_pos);
--- src/regles.c
+++ src/regles.c
@@ -1,31 +1,45 @@
 #include "../inc/regles.h"
 
-int life_points = 5;
+
+/*======================== Variables ===========================*/
+
+int life_points = 5;    /* amount of initial life points */
 double end_corridor;
 
+
+/*======================== Functions ===========================*/
+
+/* Adds a single user life point */
 void addLife(){
     life_points ++;
 }
 
 
+/* Initialize the length of the corridor */
 void initLengthCorridor(){
     end_corridor = randRange(40, 200);
 }
 
+
+/* Remove a single user life point */
 void removeLife(){
     life_points --;
 }
 
 
+/* Checks if the player is still alive */
 int isAlive(){
     return life_points > 0;
 }
 
 
+/* Retrieves the end information on the corridor axis */
 double endCorridor(){
     return end_corridor;
 }
 
+
+/* Checks if the racket position given is out of the corridor */
 int isCorridorEnd(double racket_pos){
     return -racket_pos >= end_corridor; 
 }

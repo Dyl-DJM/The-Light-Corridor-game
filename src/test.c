b03eb5a83a7d78a5c4ee5adb1ba6c3791d0ccdd9
Dylan DE JESUS <dyl.dejesusmilitar@gmail.com>
2023-04-28 19:11:30 +0200

    Test module cleared

--- src/test.c
+++ src/test.c
@@ -3,13 +3,12 @@
 
 int main(void)
 {
-
+    /*========================= Obstacles tests ==========================*/
     ObstacleList *list;
     RectanglePoints rect = initRect(initCoords(0, 0), initCoords(6, 6));
     RectanglePoints rect2 = initRect(initCoords(2, 4), initCoords(6, 8));
 
     list = initObstacleList();
-
     printList(*list);
 
     add(list, rect, 10);
@@ -18,9 +17,7 @@ int main(void)
     add(list, rect2, 20);
 
     printList(*list);
-
     removeObs(list, -15); /* Remove all the obstacles which are positioned behind 15 in z value in the scene*/
-
     printList(*list);
 
     int ball_pos = -19;
@@ -29,13 +26,12 @@ int main(void)
         addRandomObstacle(list, ball_pos, 1000);
         ball_pos -= 4;
     }
-
     printList(*list);
 
+    /*=========================== Bonus tests ============================*/
+
     printf("\n\n=== Bonus part ===\n\n");
-
     BonusList * blist = initBonusList();
-
     printBonusList(*blist);
 
     Coords3D bcoords;
@@ -49,12 +45,8 @@ int main(void)
     addBonus(blist, bcoords, 15);
 
     printBonusList(*blist);
-
     removeBonus(blist, -100);
-
     printBonusList(*blist);
-
-
 
     return 0;
 }
